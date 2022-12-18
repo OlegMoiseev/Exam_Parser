4be62019773db8349b1278cb6a3f3dae0674446f
Олег Моисеев <olegmoiseev2698@gmail.com>
2022-12-18 08:33:53 +0300

    Changed function name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import parser
+import parser_HH
 
 # full list of variants:
 # 'Kafka', 'Airflow', 'Apache Spark', 'Apach Beam', 'MLflow', 'Kuberflow', 'Hadoop', 'DVC', 'Feast'
 
-parser.get_vacancies('Kafka', 'Airflow', 'Apache Spark', save_2_csv=True)
+parser_HH.get_vacancies('Kafka', 'Airflow', 'Apache Spark', save_2_csv=True)
